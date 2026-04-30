d3a7b231211a8caacc53ee274b5e9c306bb246b6
weamaldaja-33 <weamaldaja3@gmail.com>
2026-04-30 20:23:14 +0300

    add medicines data and main page

---
+++ src/MainPage.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import medicines from "./medicines";
+// component
+function MainPage(){
+const [search ,setSearch]=useState("");
+const[sortBy,setSortBy]=useState("name");
+const filtered = medicines
+    .filter((m) =>
+      m.name.toLowerCase().includes(search.toLowerCase()) ||
+      m.category.toLowerCase().includes(search.toLowerCase())
+    )
+    //sort the filtered list
+    .sort((a, b) => {
+      //if user chose price sort from cheapest to most expensive
+        if (sortBy === "price") return a.price - b.price;
+        //otherwise sort alphabatically by name
+      return a.name.localeCompare(b.name);
+    });
+//what user see on the screen
+return(
+<div>
+<h1>Pharmacy Products</h1>
+<input
+        type="text"
+        placeholder="Search medicines..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+<select
+        value={sortBy}
+        onChange={(e) => setSortBy(e.target.value)}
+      >
+        <option value="name">Sort by Name</option>
+        <option value="price">Sort by Price</option>
+      </select>
+
+<table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Brand</th>
+            <th>Price (JD)</th>
+          </tr>
+        </thead>
+<tbody>
+          {filtered.map((m) => (
+            <tr key={m.id}>
+              <td>{m.id}</td>
+              <td>{m.name}</td>
+              <td>{m.category}</td>
+              <td>{m.brand}</td>
+              <td>{m.price}</td>
+            </tr>
+          ))}
+        </tbody>
+</table>
+</div>
+);
+}
+export default MainPage;
